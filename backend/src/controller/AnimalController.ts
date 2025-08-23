7920063171e3662322862654897b1a7ee02fe95b
Behera Manav <manavbehera123@gmail.com>
2025-08-23 20:28:46 +0530

    feat(animal): implement AnimalController and AnimalRoute for CRUD operations

---
+++ backend/src/controller/AnimalController.ts
@@ -0,0 +1,188 @@
+import { Request, Response } from "express";
+import AnimalModel, {
+  AnimalValidation,
+  UpdateAnimalValidation,
+} from "../model/AnimalModel";
+import { sendResponse } from "../utilies/SendResponse";
+import { logger } from "../utilies/Logger";
+export class AnimalController {
+  async createAnimal(req: Request, res: Response): Promise<void> {
+    const { body } = req;
+    try {
+      const isValid = AnimalValidation.safeParse(body);
+      if (!isValid.success) {
+        logger.error("Validation error", isValid.error);
+        return sendResponse({
+          res,
+          statusCode: 400,
+          success: false,
+          message: "Validation error",
+          data: {},
+        });
+      }
+      const isExist = await AnimalModel.findOne({ name: body.name });
+      if (isExist) {
+        logger.error("Animal already exists", { name: body.name });
+        return sendResponse({
+          res,
+          statusCode: 409,
+          success: false,
+          message: "Animal already exists",
+          data: { name: body.name },
+        });
+      }
+      const newAnimal = await AnimalModel.create(body);
+      return sendResponse({
+        res,
+        statusCode: 201,
+        success: true,
+        message: "Animal created successfully",
+        data: { animal: newAnimal },
+      });
+    } catch (error) {
+      logger.error("Error creating animal -> (createAnimal)", error);
+      sendResponse({
+        res,
+        statusCode: 500,
+        success: false,
+        message: "Error creating animal",
+        data: {},
+      });
+    }
+  }
+
+  async getAllAnimals(req: Request, res: Response): Promise<void> {
+    try {
+      const animals = await AnimalModel.find();
+      return sendResponse({
+        res,
+        statusCode: 200,
+        success: true,
+        message: "Animals retrieved successfully",
+        data: { animals },
+      });
+    } catch (error) {
+      logger.error("Error retrieving animals -> (getAllAnimals)", error);
+      sendResponse({
+        res,
+        statusCode: 500,
+        success: false,
+        message: "Error retrieving animals",
+        data: {},
+      });
+    }
+  }
+
+  async getAnimalById(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    try {
+      const animal = await AnimalModel.findById(id);
+      if (!animal) {
+        logger.error("Animal not found", { id });
+        return sendResponse({
+          res,
+          statusCode: 404,
+          success: false,
+          message: "Animal not found",
+          data: {},
+        });
+      }
+      return sendResponse({
+        res,
+        statusCode: 200,
+        success: true,
+        message: "Animal retrieved successfully",
+        data: { animal },
+      });
+    } catch (error) {
+      logger.error("Error retrieving animal -> (getAnimalById)", error);
+      sendResponse({
+        res,
+        statusCode: 500,
+        success: false,
+        message: "Error retrieving animal",
+        data: {},
+      });
+    }
+  }
+
+  async updateAnimal(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    const { body } = req;
+    try {
+      const isValid = UpdateAnimalValidation.safeParse(body);
+      if (!isValid.success) {
+        logger.error("Validation error", isValid.error);
+        return sendResponse({
+          res,
+          statusCode: 400,
+          success: false,
+          message: "Validation error",
+          data: {},
+        });
+      }
+      const animal = await AnimalModel.findByIdAndUpdate(id, body, {
+        new: true,
+      });
+      if (!animal) {
+        logger.error("Animal not found", { id });
+        return sendResponse({
+          res,
+          statusCode: 404,
+          success: false,
+          message: "Animal not found",
+          data: {},
+        });
+      }
+      return sendResponse({
+        res,
+        statusCode: 200,
+        success: true,
+        message: "Animal updated successfully",
+        data: { animal },
+      });
+    } catch (error) {
+      logger.error("Error updating animal -> (updateAnimal)", error);
+      sendResponse({
+        res,
+        statusCode: 500,
+        success: false,
+        message: "Error updating animal",
+        data: {},
+      });
+    }
+  }
+
+  async deleteAnimal(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    try {
+      const animal = await AnimalModel.findByIdAndDelete(id);
+      if (!animal) {
+        logger.error("Animal not found", { id });
+        return sendResponse({
+          res,
+          statusCode: 404,
+          success: false,
+          message: "Animal not found",
+          data: {},
+        });
+      }
+      return sendResponse({
+        res,
+        statusCode: 204,
+        success: true,
+        message: "Animal deleted successfully",
+        data: {},
+      });
+    } catch (error) {
+      logger.error("Error deleting animal -> (deleteAnimal)", error);
+      sendResponse({
+        res,
+        statusCode: 500,
+        success: false,
+        message: "Error deleting animal",
+        data: {},
+      });
+    }
+  }
+}
