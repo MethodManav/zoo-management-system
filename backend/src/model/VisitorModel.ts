f84cd16762052a157fc5751f012056890f23bc19
Behera Manav <manavbehera123@gmail.com>
2025-08-24 23:07:00 +0530

    feat(visitor): add visitorType field to Visitor schema and model

--- backend/src/model/VisitorModel.ts
+++ backend/src/model/VisitorModel.ts
@@ -1,9 +1,17 @@
 import mongoose, { Schema } from "mongoose";
 import z, { number } from "zod";
 
+export enum VisitorType {
+  INDIVIDUAL = "Individual",
+  FAMILY = "Family",
+  FOREIGN_TOURIST = "Foreign Tourist",
+  SCHOOL = "School",
+}
+
 export const VisitorSchema = z.object({
   name: z.string().min(2).max(100),
   mobile: z.string().min(10).max(15),
+  visitorType: z.nativeEnum(VisitorType),
   memberOfFamily: z.number().min(1),
   dateOfBooking: z.string(),
   amount: z.number().min(0),
@@ -14,6 +22,11 @@ export type VisitorValidation = z.infer<typeof VisitorSchema>;
 const VisitorSchemaMongoose = new Schema({
   name: { type: String, required: true, minlength: 2, maxlength: 100 },
   mobile: { type: String, required: true, minlength: 10, maxlength: 15 },
+  visitorType: {
+    type: String,
+    enum: Object.values(VisitorType),
+    required: true,
+  },
   memberOfFamily: { type: Number, required: true, min: 1 },
   dateOfBooking: { type: String, required: true },
   amount: { type: Number, required: true, min: 0 },
